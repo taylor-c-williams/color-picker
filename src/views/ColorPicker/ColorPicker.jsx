a6aa1984e96dbbe7dd270e1b5a777ce16b80a2b5
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-12 11:57:39 -0800

    useAffirmation implemented

--- src/views/ColorPicker/ColorPicker.jsx
+++ src/views/ColorPicker/ColorPicker.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import useAffirmation from '../../hooks/useAffirmation';
 import Display from '../../components/Display/Display';
 import styles from './ColorPicker.css';
 
@@ -6,21 +7,24 @@ export default function ColorPicker() {
   const [fgColor, setFgColor] = useState('#ffcc00');
   const [bgColor, setBgColor] = useState('#212121');
   const [content, setContent] = useState('Hello, world!');
-  const [didChangeColor, setDidChangeColor] = useState(false);
-  const [affirmation, setAffirmation] = useState('');
+  const affirmation = useAffirmation(bgColor, fgColor);
 
-  useEffect(() => {
-    const affirmations = [
-      'Great choice!',
-      'I love that color!',
-      'Looks good!',
-      'What a great color combo!',
-      'Ooh la la, so fancy',
-    ];
-    // Generate a random whole number between 0 and the last index of the array
-    const randomIndex = Math.floor(Math.random() * affirmations.length);
-    setAffirmation(affirmations[randomIndex]);
-  }, [bgColor, fgColor]);
+  // const [affirmation, setAffirmation] = useState('');
+
+  // useEffect(() => {
+  //   const affirmations = [
+  //     'Great choice!',
+  //     'I love that color!',
+  //     'Looks good!',
+  //     'What a great color combo!',
+  //     'Ooh la la, so fancy',
+  //   ];
+  //   // Generate a random whole number between 0 and the last index of the array
+  //   const randomIndex = Math.floor(Math.random() * affirmations.length);
+  //   setAffirmation(affirmations[randomIndex]);
+  // }, [bgColor, fgColor]);
+
+  const [didChangeColor, setDidChangeColor] = useState(false);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
