4389b6f335c5e7900eb44669c9421804661a4292
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-12 12:14:18 -0800

    useColorPicker hook implemented, tests passing

--- src/hooks/useColorPicker.js
+++ src/hooks/useColorPicker.js
@@ -1,3 +1,28 @@
+import { useState } from 'react';
 export default function useColorPicker() {
-  // TODO: Implement me!
+  const [didChangeColor, setDidChangeColor] = useState(false);
+  const [fgColor, setFgColor] = useState('#ffcc00');
+  const [bgColor, setBgColor] = useState('#212121');
+  const [content, setContent] = useState('Hello, world!');
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'fgColor':
+        setFgColor(value);
+        setDidChangeColor(true);
+        break;
+      case 'bgColor':
+        setBgColor(value);
+        setDidChangeColor(true);
+        break;
+      case 'content':
+        setDidChangeColor(false);
+        setContent(value);
+        break;
+      default:
+        break;
+    }
+  };
+  return [didChangeColor, bgColor, fgColor, content, handleChange];
 }
--- src/views/ColorPicker/ColorPicker.jsx
+++ src/views/ColorPicker/ColorPicker.jsx
@@ -1,50 +1,12 @@
-import { useEffect, useState } from 'react';
+import useColorPicker from '../../hooks/useColorPicker';
 import useAffirmation from '../../hooks/useAffirmation';
 import Display from '../../components/Display/Display';
 import styles from './ColorPicker.css';
 
 export default function ColorPicker() {
-  const [fgColor, setFgColor] = useState('#ffcc00');
-  const [bgColor, setBgColor] = useState('#212121');
-  const [content, setContent] = useState('Hello, world!');
   const affirmation = useAffirmation(bgColor, fgColor);
-
-  // const [affirmation, setAffirmation] = useState('');
-
-  // useEffect(() => {
-  //   const affirmations = [
-  //     'Great choice!',
-  //     'I love that color!',
-  //     'Looks good!',
-  //     'What a great color combo!',
-  //     'Ooh la la, so fancy',
-  //   ];
-  //   // Generate a random whole number between 0 and the last index of the array
-  //   const randomIndex = Math.floor(Math.random() * affirmations.length);
-  //   setAffirmation(affirmations[randomIndex]);
-  // }, [bgColor, fgColor]);
-
-  const [didChangeColor, setDidChangeColor] = useState(false);
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    switch (name) {
-      case 'fgColor':
-        setFgColor(value);
-        setDidChangeColor(true);
-        break;
-      case 'bgColor':
-        setBgColor(value);
-        setDidChangeColor(true);
-        break;
-      case 'content':
-        setDidChangeColor(false);
-        setContent(value);
-        break;
-      default:
-        break;
-    }
-  };
+  const [didChangeColor, bgColor, fgColor, content, handleChange] =
+    useColorPicker();
 
   return (
     <>
